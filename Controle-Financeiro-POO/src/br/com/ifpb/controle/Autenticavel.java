928e78a2649d433e1ee056144f08eb6e89eca98c
Aguirre Sabino <aguirresabino187@gmail.com>
2017-08-30 17:29:44 -0300

    Implementando interface autenticavel em usuário

--- Controle-Financeiro-POO/src/br/com/ifpb/controle/Autenticavel.java
+++ Controle-Financeiro-POO/src/br/com/ifpb/controle/Autenticavel.java
@@ -1,5 +1,5 @@
 package br.com.ifpb.controle;
 
 public interface Autenticavel {
-    public boolean autentica(String login, String senha);
+    public boolean autentica(String email, String senha);
 }
